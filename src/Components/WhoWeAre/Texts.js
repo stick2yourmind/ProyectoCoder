b6d561bb09e697ea8d7d5d3566c197f1c88ea1ee
Max Saravia <saravia.max.stk2@gmail.com>
2022-07-01 15:08:34 -0300

    fix: grammar errors
    fix: grammar errors

--- src/Components/WhoWeAre/Texts.js
+++ src/Components/WhoWeAre/Texts.js
@@ -4,7 +4,7 @@ import styles from './WhoWeAre.module.css';
 const Texts = () => {
   return (
     <div className={styles.TxtContainer}>
-      <h2 id='quienesSomos'>¿Que hacemos?</h2>
+      <h2 id='quienesSomos'>¿Qué hacemos?</h2>
       <div className={styles.Texts}>
         <p>Creamos y producimos contenidos audiovisuales especialmente para redes sociales.</p>
         <p>Trabajamos en el desarollo de piezas y proyectos audiovisuales junto a equipos de comunicación institucional.</p>
